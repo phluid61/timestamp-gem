805d24cfea05f83b684c6e5b398e58a739ada6c7
Matthew Kerwin <matthew@kerwin.net.au>
2013-03-15 23:44:09 +1000

    added Time.timestamp alias

--- ext/timestamp/Timestamp.c
+++ ext/timestamp/Timestamp.c
@@ -4,11 +4,13 @@
 /*
  *  call-seq:
  *     Time.current_timestamp -> int
+ *     Time.timestamp         -> int
  *
  *  Returns the current time as an integer number of seconds
  *  since the Epoch.
  *
  *     Time.current_timestamp  #=> 1363352771
+ *     Time.timestamp          #=> 1363352771
  */
 
 static VALUE
@@ -19,4 +21,5 @@ time_s_current_timestamp(VALUE klass)
 
 void Init_timestamp() {
     rb_define_singleton_method(rb_cTime, "current_timestamp", time_s_current_timestamp, 0);
+    rb_define_singleton_method(rb_cTime, "timestamp", time_s_current_timestamp, 0);
 }
--- test/test_timestamp.rb
+++ test/test_timestamp.rb
@@ -5,10 +5,12 @@ require "#{File.dirname File.dirname(__FILE__)}/lib/timestamp"
 
 class Test_timestamp < Test::Unit::TestCase
 	def test_timestamp
-		i = Time.now.to_i
-		j = Time.current_timestamp
-		d = (j-i).abs
-		assert( d < 1, "Unexpectedly highly difference #{d}" )
+		int = Time.now.to_i
+		stp = [Time.timestamp, Time.current_timestamp]
+		stp.each do |t|
+			d = (t-int).abs
+			assert( d <= 1, "Unexpectedly highly difference #{d}" )
+		end
 	end
 end
 
