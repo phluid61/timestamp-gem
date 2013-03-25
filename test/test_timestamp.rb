ece2fdb6eea1d68d932e862e9a9cd9e2720156a0
Matthew Kerwin <matthew@kerwin.net.au>
2013-03-26 07:37:27 +1000

    updated tests for 1.0

--- test/test_timestamp.rb
+++ test/test_timestamp.rb
@@ -5,17 +5,22 @@ require "#{File.dirname File.dirname(__FILE__)}/lib/timestamp"
 
 class Test_timestamp < Test::Unit::TestCase
 	def test_timestamp
-		int = Time.now.to_i
-		stp = [Time.timestamp, Time.current_timestamp]
-		stp.each do |t|
-			d = (t-int).abs
-			assert( d <= 1, "Unexpectedly highly difference #{d}" )
+		stp = [Time.timestamp, Time.timestamp, Time.timestamp]
+		stp.inject do |m,t|
+			d = t - m
+			assert( d > 0, "Retrograde timestamp #{m} to #{t}" )
 		end
 	end
-	def test_microtime
-		stp = Time.microtime
-		int = Time.now.to_f
+	def test_unix_timestamp
+		int = Time.now.to_i
+		stp = Time.unix_timestamp
 		d = (stp-int).abs
+		assert( d <= 1, "Unexpectedly highly difference #{d}" )
+	end
+	def test_unix_microtime
+		flt = Time.now.to_f
+		stp = Time.unix_microtime
+		d = (stp-flt).abs
 		assert( d <= 1, "Unexpectedly highly difference #{d}" )
 	end
 end
