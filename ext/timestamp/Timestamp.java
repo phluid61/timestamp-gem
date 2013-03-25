febbe39e0a678514b9612456952801ad7006926c
Matthew Kerwin <matthew@kerwin.net.au>
2013-03-26 07:12:14 +1000

    added jruby stuffs

---
+++ ext/timestamp/Timestamp.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2013, Matthew Kerwin
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution. 
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package au.net.kerwin.jruby;
+
+import org.jruby.Ruby;
+import org.jruby.RubyClass;
+import org.jruby.RubyFixnum;
+import org.jruby.RubyFloat;
+import org.jruby.anno.JRubyMethod;
+import org.jruby.runtime.builtin.IRubyObject;
+import org.jruby.runtime.load.BasicLibraryService;
+
+public class Timestamp implements BasicLibraryService {
+    private Ruby runtime;
+
+    public boolean basicLoad(Ruby runtime) throws IOException {
+	this.runtime = runtime;
+	RubyClass rb_cTime = runtime.getClass("Time");
+	rb_cTime.defineAnnotatedMethods(Timestamp.class);
+	return true;
+    }
+
+    /*
+     *  call-seq:
+     *     Time.current_timestamp -> int
+     *     Time.timestamp         -> int
+     *
+     *  Returns the current time as an integer number of seconds
+     *  since the Epoch.
+     *
+     *     Time.current_timestamp  #=> 1363352771
+     *     Time.timestamp          #=> 1363352771
+     */
+
+    @JRubyMethod( name = "current_timestamp", alias = { "timestamp" } )
+    public IRubyObject current_timestamp() {
+	return new RubyFixnum(this.runtime, System.currentTimeMillis / 1000);
+    }
+
+    /*
+     *  call-seq:
+     *     Time.microtime  -> float
+     *
+     *  Returns the current time as a floating-point number of seconds
+     *  since the Epoch.
+     *
+     *     Time.microtime  #=> 1363352771.315240
+     */
+
+    @JRubyMethod( name = "microtime" )
+    public IRubyObject microtime() {
+	return new RubyFloat(this.runtime, System.currentTimeMillis / 1000.0);
+    }
+}
+
--- timestamp.gemspec
+++ timestamp.gemspec
@@ -1,14 +1,20 @@
 Gem::Specification.new do |s|
   s.name = 'timestamp'
-  s.version = '0.3.0'
-  s.date = '2013-03-15'
+  s.version = '0.3.1'
+  s.date = '2013-03-21'
   s.summary = 'Time.current_timestamp'
   s.description = 'Defines Time.current_timestamp , from https://bugs.ruby-lang.org/issues/8096'
   s.authors = ['Matthew Kerwin']
   s.email = ['matthew@kerwin.net.au']
 
-  s.files = ['ext/timestamp/Timestamp.c']
-  s.extensions = ['ext/timestamp/extconf.rb']
+  files = Dir['ext/timestamp/*.{c,java,rb}']
+	if RUBY_PLATFORM =~ /java/
+		s.platform = 'java'
+		files << 'lib/timestamp.jar'
+	else
+    s.extensions = ['ext/timestamp/extconf.rb']
+	end
+	s.files = files
 
   s.homepage = 'http://rubygems.org/gems/timestamp'
   s.license = 'Simplified BSD License'
