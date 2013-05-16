ec8d0713c0dca3972667e690d2aa2f2474da41a8
Matthew Kerwin <matthew@kerwin.net.au>
2013-05-16 10:53:37 +1000

    more travis-ci testing

--- test/test_timestamp.rb
+++ test/test_timestamp.rb
@@ -9,7 +9,7 @@ if defined?(JRUBY_VERSION)
 	$timestamp = Java::Default::Timestamp.new
 	$timestamp.java_send :basicLoad, [org.jruby.Ruby], org.jruby.Ruby.getGlobalRuntime
 
-	class Test_timestamp < Test::Unit::TestCase
+	class Test_timestamp_java < Test::Unit::TestCase
 		def test_timestamp
 			stp = 1000.times.map{$timestamp.java_send :timestamp, []}
 			stp.inject do |m,t|
@@ -30,36 +30,35 @@ if defined?(JRUBY_VERSION)
 			assert( d <= 1, "Unexpectedly highly difference #{d}" )
 		end
 	end
-
 else
 	require "#{File.dirname File.dirname(__FILE__)}/lib/timestamp"
+end
 
-	class Test_timestamp < Test::Unit::TestCase
-		def test_timestamp
-			stp = 1000.times.map{Time.timestamp}
-			stp.inject do |m,t|
-				assert( t >= m, "Retrograde timestamp #{m} to #{t}" )
-				t
-			end
-		end
-		def test_unix_time
-			int = Time.now.to_i
-			stp = Time.unix_time
-			d = (stp-int).abs
-			assert( d <= 1, "Unexpectedly highly difference #{d}" )
-		end
-		def test_unix_timestamp
-			int = Time.now.to_i
-			stp = Time.unix_timestamp
-			d = (stp-int).abs
-			assert( d <= 1, "Unexpectedly highly difference #{d}" )
-		end
-		def test_unix_microtime
-			flt = Time.now.to_f
-			stp = Time.unix_microtime
-			d = (stp-flt).abs
-			assert( d <= 1, "Unexpectedly highly difference #{d}" )
+class Test_timestamp < Test::Unit::TestCase
+	def test_timestamp
+		stp = 1000.times.map{Time.timestamp}
+		stp.inject do |m,t|
+			assert( t >= m, "Retrograde timestamp #{m} to #{t}" )
+			t
 		end
 	end
-
+	def test_unix_time
+		int = Time.now.to_i
+		stp = Time.unix_time
+		d = (stp-int).abs
+		assert( d <= 1, "Unexpectedly highly difference #{d}" )
+	end
+	def test_unix_timestamp
+		int = Time.now.to_i
+		stp = Time.unix_timestamp
+		d = (stp-int).abs
+		assert( d <= 1, "Unexpectedly highly difference #{d}" )
+	end
+	def test_unix_microtime
+		flt = Time.now.to_f
+		stp = Time.unix_microtime
+		d = (stp-flt).abs
+		assert( d <= 1, "Unexpectedly highly difference #{d}" )
+	end
 end
+
