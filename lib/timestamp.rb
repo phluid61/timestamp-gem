cb6fb67b7060c5b67c23d69c2e1c93dec18de6f7
Matthew Kerwin <matthew.kerwin@qut.edu.au>
2018-07-24 15:38:38 +1000

    change how jruby loads

--- lib/timestamp.rb
+++ lib/timestamp.rb
@@ -1,25 +1,24 @@
 
 if defined?(JRUBY_VERSION)
-	require 'jruby'
-	require File.expand_path('../timestamp.jar', __FILE__)
-	require 'timestamp'
+  require 'jruby'
+  require_relative 'timestamp.jar'
 
-	class Time
-		@@timestamp = Java::Default::Timestamp.new.tap{|ts| ts.java_send :basicLoad, [org.jruby.Ruby], org.jruby.Ruby.getGlobalRuntime }
-		class << self
-			def timestamp
-				@@timestamp.java_send :timestamp, []
-			end
-			def unix_timestamp
-				@@timestamp.java_send :unix_timestamp, []
-			end
-			alias :unix_time :unix_timestamp
-			def unix_microtime
-				@@timestamp.java_send :unix_microtime, []
-			end
-		end
-	end
+  class Time
+    @@timestamp = Java::Default::Timestamp.new.tap{|ts| ts.java_send :basicLoad, [org.jruby.Ruby], org.jruby.Ruby.getGlobalRuntime }
+    class << self
+      def timestamp
+        @@timestamp.java_send :timestamp, []
+      end
+      def unix_timestamp
+        @@timestamp.java_send :unix_timestamp, []
+      end
+      alias :unix_time :unix_timestamp
+      def unix_microtime
+        @@timestamp.java_send :unix_microtime, []
+      end
+    end
+  end
 else
-	require_relative "timestamp.#{RbConfig::CONFIG['DLEXT']}"
+  require_relative "timestamp.#{RbConfig::CONFIG['DLEXT']}"
 end
 
--- test/test_timestamp.rb
+++ test/test_timestamp.rb
@@ -4,30 +4,30 @@ $VERBOSE = true
 require_relative '../lib/timestamp'
 
 class Test_timestamp < Test::Unit::TestCase
-	def test_timestamp
-		stp = 1000.times.map{Time.timestamp}
-		stp.inject do |m,t|
-			assert( t >= m, "Retrograde timestamp #{m} to #{t}" )
-			t
-		end
-	end
-	def test_unix_time
-		int = Time.now.to_i
-		stp = Time.unix_time
-		d = (stp-int).abs
-		assert( d <= 1, "Unexpectedly highly difference #{d}" )
-	end
-	def test_unix_timestamp
-		int = Time.now.to_i
-		stp = Time.unix_timestamp
-		d = (stp-int).abs
-		assert( d <= 1, "Unexpectedly highly difference #{d}" )
-	end
-	def test_unix_microtime
-		flt = Time.now.to_f
-		stp = Time.unix_microtime
-		d = (stp-flt).abs
-		assert( d <= 1, "Unexpectedly highly difference #{d}" )
-	end
+  def test_timestamp
+    stp = 1000.times.map{Time.timestamp}
+    stp.inject do |m,t|
+      assert( t >= m, "Retrograde timestamp #{m} to #{t}" )
+      t
+    end
+  end
+  def test_unix_time
+    int = Time.now.to_i
+    stp = Time.unix_time
+    d = (stp-int).abs
+    assert( d <= 1, "Unexpectedly highly difference #{d}" )
+  end
+  def test_unix_timestamp
+    int = Time.now.to_i
+    stp = Time.unix_timestamp
+    d = (stp-int).abs
+    assert( d <= 1, "Unexpectedly highly difference #{d}" )
+  end
+  def test_unix_microtime
+    flt = Time.now.to_f
+    stp = Time.unix_microtime
+    d = (stp-flt).abs
+    assert( d <= 1, "Unexpectedly highly difference #{d}" )
+  end
 end
 
